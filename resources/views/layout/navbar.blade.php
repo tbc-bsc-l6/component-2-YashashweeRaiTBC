fa17db4c9e0d6c8cebc1f13639ce0c380934a142
Yashashwee <yashashwee.rai@gmail.com>
2025-01-09 21:42:06 +0545

    Add layout for the navigation bar

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="row g-0">
+    <div class="col-12 g-0 pe-4 m-0" style="background: #D09683; height: 90px;">
+        <nav class="navbar navbar-light" style="height: 100%; display: flex; justify-content: space-between; align-items: center; padding: 0 15px;">
+            <a class="navbar-brand" href="/">
+                <img style="max-height: 80px; max-width: 80px;" src="/logo.png" title="Cozy Nook">
+            </a>
+            <a class="" href="/login">LOGIN</a>
+        </nav>
+    </div>
+    <div class="col-12 text-center g-0 border-bottom-brown border-top-browm background-brown">
+        <img style="max-height: 420px; max-width: 1250px;" src="/header.png" title="The main header">
+    </div>
+</div>
