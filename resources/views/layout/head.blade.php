490e4c21f44e242c06024a2279739ab176fd2d9e
Yashashwee <yashashwee.rai@gmail.com>
2025-01-09 21:40:58 +0545

    Add layout for the head section

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,7 @@
+<title> {{ $title }} </title>
+
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Borel&display=swap" rel="stylesheet">
