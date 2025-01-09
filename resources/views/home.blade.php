2dfa4bc75fbb8300de38982673322ff2aaafd7b7
Yashashwee <yashashwee.rai@gmail.com>
2025-01-09 21:45:30 +0545

    Add main page structure with navbar and content section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @include("layout.head")
+    </head>
+
+    <body style="background: #D09683">
+        
+        @include("layout.navbar")
+        
+        <div class="container ps-5 pe-5">
+            <div class="row">
+                <div class="col-12 p-2 text-center mt-4 mb-4 border-bottom-brown">
+                    <h1 style="font-family: 'Borel'; color: #330000;">Cozy Nook</h1>
+                    <p style="font-family: 'Borel'; color: #330000;"><i>Share your love for books</i></p>
+                </div>
+            </div>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
