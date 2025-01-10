09b8920dcb889f66ba4eb70b6f59ebec7d6c055e
Yashashwee <yashashwee.rai@gmail.com>
2025-01-10 17:49:28 +0545

    Add layout for footer.

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,21 @@
+<div class="row g-0 mt-5">
+    <div class="col-12 g-0 ps-2 pe-4 m-0 border-top-brown" style="background: #330000; height: 165px;">
+        <footer class="d-flex flex-column flex-lg-row justify-content-between align-items-center h-100 px-4">
+            <!-- Footer Logo and Text -->
+            <div class="d-flex align-items-center col-lg-6 col-12">
+                <a class="footer-logo" href="/">
+                    <img style="max-height: 140px; max-width: 140px; margin-top: 25px;" src="/logo_peach.png" title="Cozy Nook">
+                </a>
+                <span class="text-peach fs-4" style="font-family: 'Borel'; margin-top: 20px; margin-left: -40px;">Cozy Nook</span>
+            </div>
+
+            <!-- Footer Links -->
+            <div class="footer-links d-flex align-items-center col-lg-6 col-12 justify-content-center justify-content-lg-end">
+                <a class="me-3 text-peach" href="/about-us">About Us</a>
+                <a class="me-3 text-peach" href="/contact">Contact</a>
+                <a class="me-3 text-peach" href="/privacy-policy">Privacy Policy</a>
+            </div>
+        </footer>
+    </div>
+</div>
+
