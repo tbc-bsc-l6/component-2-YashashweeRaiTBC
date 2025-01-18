13807e6fad20790ea10e355b70a2dce8e596a6fa
Yashashwee <yashashwee.rai@gmail.com>
2025-01-18 20:44:42 +0545

    Add BlogsRepository for managing blog data.

---
+++ app/Modules/Blogs/BlogsRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Blogs;
+
+use App\Http\Requests\BlogsUpdateRequest;
+use App\Models\Blogs;
+use App\Models\BlogTags;
+
+class BlogsRepository
+{
+    const RECENT_BLOGS_LIMIT = 5;
+    
+    public function getTotalCount(): int
+    {
+        return Blogs::all()->count();
+    }
+
+    public function UIList(int $page, int $pageLength, array $filters = []): array
+    {
+        if ($filters !== []) {
+            return Blogs::with(["tags"])
+                ->where($filters)
+                ->where("id", ">", 0)
+                ->limit($pageLength)
+                ->offset(($page - 1) * $pageLength)
+                ->get()
+                ->toArray();
+        }
+
+        return Blogs::with(["tags"])
+            ->where("id", ">", 0)
+            ->limit($pageLength)
+            ->offset(($page - 1) * $pageLength)
+            ->get()
+            ->toArray();
+
+    }
+
+    public function UIListRecent(): array
+    {
+        return Blogs::with(["tags"])
+            ->where("id", ">", 0)
+            ->limit(self::RECENT_BLOGS_LIMIT)
+            ->orderBy("created_at", "desc")
+            ->get()
+            ->toArray();
+    }
+ 
+}
