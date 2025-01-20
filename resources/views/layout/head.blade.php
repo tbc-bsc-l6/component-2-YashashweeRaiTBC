e6c78cf8136361c32189bca0c2e4ba3bf8fa8d3f
Yashashwee <yashashwee.rai@gmail.com>
2025-01-20 20:56:06 +0545

    Remove redundant files.

--- resources/views/layout/head.blade.php
+++
@@ -1,7 +0,0 @@
-<title> {{ $title }} </title>
-
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Borel&display=swap" rel="stylesheet">
